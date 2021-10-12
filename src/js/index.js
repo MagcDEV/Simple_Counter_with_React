ebfcd916f0a0507acd44e663ec8f00149e7c5bb9
MagcDEV <manuel117g@gmail.com>
2021-10-12 08:41:22 -0400

    final

--- src/js/index.js
+++ src/js/index.js
@@ -8,8 +8,47 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import Home from "./component/home.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import PropTypes from "prop-types";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+//create your first component
+const SimpleCounter = props => {
+	return (
+		<div className="bigCounter">
+			<div className="calendar">
+				<FontAwesomeIcon icon={faClock} />
+			</div>
+			<div className="four">{props.digitFour}</div>
+			<div className="three">{props.digitThree}</div>
+			<div className="two">{props.digitTwo}</div>
+			<div className="one">{props.digitOne}</div>
+		</div>
+	);
+};
+
+SimpleCounter.prototype = {
+	digitOne: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitFour: PropTypes.number
+};
+let counter = 0;
+
+setInterval(function() {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	console.log(four, three, two, one);
+	counter++;
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
